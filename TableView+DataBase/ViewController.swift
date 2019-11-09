76ecd1279d7d47f602cd5e3030a1b4472feee5d5
byLLIPyT <alex-tt@mail.ru>
2019-11-10 01:13:47 +0300

    Initial Commit

---
+++ TableView+DataBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView+DataBase
+//
+//  Created by Александр Уткин on 10/11/2019.
+//  Copyright © 2019 Александр Уткин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
